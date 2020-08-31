f4c93888bd788d598b38d877b03ac45870863ad9
Yusuke Yoshino <y.yoshino1011@gmail.com>
2020-08-31 19:53:48 +0900

    add props to congrats component

--- word-guess-game/src/Congrats.js
+++ word-guess-game/src/Congrats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Congrats = (props) => {
   if (props.success) {
@@ -10,6 +11,10 @@ const Congrats = (props) => {
   } else {
     return <div data-test="component-congrats" />;
   }
+};
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired,
 };
 
 export default Congrats;
--- word-guess-game/src/Congrats.test.js
+++ word-guess-game/src/Congrats.test.js
@@ -1,18 +1,23 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import { checkProps } from "./test/testUtils";
 
 import { findByTestAttr } from "./test/testUtils";
 import Congrats from "./Congrats";
+import { charCodeAt } from "prop-types/lib/ReactPropTypesSecret";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProp = { success: false };
+
 const setup = (props = {}) => {
-  return shallow(<Congrats {...props} />);
+  const setupProps = { ...defaultProp, ...props };
+  return shallow(<Congrats {...setupProps} />);
 };
 
 test("render without error", () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
@@ -27,4 +32,9 @@ test('render non-empty message when "success" props is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
+});
+
+test("does not throw warnings with expected props", () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
